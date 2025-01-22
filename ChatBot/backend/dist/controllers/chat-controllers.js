7ed77d31eb7f40be01494d2b7d79912a6576045c
NaveenNekkanti <ndurganaveen11045@gmail.com>
2025-01-22 16:27:51 +0530

    first commit

---
+++ ChatBot/backend/dist/controllers/chat-controllers.js
@@ -0,0 +1,70 @@
+import User from "../models/User.js";
+import { configureOpenAI } from "../config/openai-config.js";
+import { OpenAIApi } from "openai";
+export const generateChatCompletion = async (req, res, next) => {
+    const { message } = req.body;
+    try {
+        // Verify user authentication
+        const user = await User.findById(res.locals.jwtData.id);
+        if (!user) {
+            return res.status(401).json({ message: "User not registered or token malfunctioned" });
+        }
+        // Prepare chat messages
+        const chats = user.chats.map(({ role, content }) => ({ role, content }));
+        chats.push({ content: message, role: "user" });
+        // Send chat messages to OpenAI for completion
+        const config = configureOpenAI();
+        const openai = new OpenAIApi(config);
+        const chatResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: chats,
+        });
+        // Update user chats with the completion
+        user.chats.push(chatResponse.data.choices[0].message);
+        await user.save();
+        // Return updated chats array
+        return res.status(200).json({ chats: user.chats });
+    }
+    catch (error) {
+        console.error("Error generating chat completion:", error);
+        return res.status(500).json({ message: "Something went wrong" });
+    }
+};
+export const sendChatsToUser = async (req, res, next) => {
+    try {
+        //user token check
+        const user = await User.findById(res.locals.jwtData.id);
+        if (!user) {
+            return res.status(401).send("User not registered OR Token malfunctioned");
+        }
+        if (user._id.toString() !== res.locals.jwtData.id) {
+            return res.status(401).send("Permissions didn't match");
+        }
+        return res.status(200).json({ message: "OK", chats: user.chats });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const deleteChats = async (req, res, next) => {
+    try {
+        //user token check
+        const user = await User.findById(res.locals.jwtData.id);
+        if (!user) {
+            return res.status(401).send("User not registered OR Token malfunctioned");
+        }
+        if (user._id.toString() !== res.locals.jwtData.id) {
+            return res.status(401).send("Permissions didn't match");
+        }
+        //@ts-ignore
+        user.chats = [];
+        await user.save();
+        return res.status(200).json({ message: "OK" });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+//# sourceMappingURL=chat-controllers.js.map
